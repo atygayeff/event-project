d6a11bfb6c88487ce4e5c14baafe989f22def216
unknown <atygayeff@gmail.com>
2024-04-22 03:54:22 +0500

    Check query

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react"
+import { getEvents } from "../services/events"
+
 export const Index = () => {
-    return <h1>Welcome</h1>
+    useEffect(() => {
+       getEvents().then((r) => {
+        console.log(r);
+       }) 
+    }, [])
+    return <h1>Welcome</h1>;
 }
