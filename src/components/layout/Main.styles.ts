12ade55a4fdda5676726b4915a58e7ab6627f148
unknown <atygayeff@gmail.com>
2024-04-22 02:46:41 +0500

    Content styling

--- src/components/layout/Main.styles.ts
+++ src/components/layout/Main.styles.ts
@@ -1,3 +1,9 @@
 import { styled } from "@mui/material";
 
 export const MainWrapper = styled('div')``;
+
+export const Content = styled('main')`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 24px;
+`;
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
-import { MainWrapper } from "./Main.styles"
+import { Content, MainWrapper } from "./Main.styles"
 
 export const MainLayout = () => {
     return <MainWrapper>
         <Header />
-        <Outlet />
+        <Content>
+            <Outlet />
+        </Content>
     </MainWrapper>
 }
